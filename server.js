17b2d13b0862ddfcba77798995baef376314b293
Vishal Antony <avishal@freshdesk.com>
2017-08-19 19:30:05 +0530

    Add projects page

--- server.js
+++ server.js
@@ -22,6 +22,12 @@ app.get('/about', (request, response) => {
   });
 });
 
+app.get('/projects', (request, response) => {
+  response.render('projects.hbs', {
+    pageTitle: 'Projects'
+  });
+});
+
 app.get('/bad', (request, response) => {
   response.send({
     code: 400,
